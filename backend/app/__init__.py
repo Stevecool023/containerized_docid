18ea90ff3ffc394acdfdc6427a9f6ff6e2f3be1c
SteveC <tallmankaranjastevey@gmail.com>
2026-04-14 11:17:42 +0300

    strop tracking docker compose

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -60,6 +60,9 @@ def create_app():
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
+        "http://127.0.0.1:3001",
+        "http://localhost:3002",
+        "http://127.0.0.1:3002",
         "https://docid.africapidalliance.org",
         "https://docid-core.africapidalliance.org",
     ])
